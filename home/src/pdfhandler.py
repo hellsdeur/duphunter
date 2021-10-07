05927e3aad1e969e8e0241c9adcbcc4f70fe2000
Helder Matos <mtshelder@gmail.com>
2021-10-07 10:35:01 -0300

    requirements.txt once again

--- home/src/pdfhandler.py
+++ home/src/pdfhandler.py
@@ -6,7 +6,7 @@ class PDFHandler:
     def __init__(self, filename, file):
         self.filename = filename
         self.text = high_level.extract_text(file)
-        # self.language = TextBlob(self.text).detect_language()
+        self.language = TextBlob(self.text).detect_language()
 
     def __str__(self):
         return self.filepath
