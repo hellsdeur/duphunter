37391470229b927d66e6093a97109be8b5bc9dad
Helder Matos <mtshelder@gmail.com>
2021-10-07 12:09:11 -0300

    Definitely the final deploy, I can't take it anymore AAAAAAAAAAAAAAAAAAAAAAA

--- home/src/pdfhandler.py
+++ home/src/pdfhandler.py
@@ -1,12 +1,10 @@
 from pdfminer import high_level
-from textblob import TextBlob
 
 
 class PDFHandler:
     def __init__(self, filename, file):
         self.filename = filename
         self.text = high_level.extract_text(file)
-        self.language = TextBlob(self.text).detect_language()
 
     def __str__(self):
         return self.filepath
