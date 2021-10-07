242b541e1715276c079dd953301f8cb73f82aa6d
Helder Matos <mtshelder@gmail.com>
2021-10-07 12:27:58 -0300

    Final deploy

--- home/src/pdfhandler.py
+++ home/src/pdfhandler.py
@@ -2,9 +2,9 @@ from pdfminer import high_level
 
 
 class PDFHandler:
-    def __init__(self, filename, file):
+    def __init__(self, filename, text_file):
         self.filename = filename
-        self.text = high_level.extract_text(file)
+        self.text = high_level.extract_text(text_file)
 
     def __str__(self):
         return self.filepath
